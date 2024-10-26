802d53fff339125488ddbb467e4b92eb0a77bba4
fldon <121126066+fldon@users.noreply.github.com>
2024-10-26 15:08:51 +0200

    Add error types

--- app/src/tftphelpdefs.h
+++ app/src/tftphelpdefs.h
@@ -9,7 +9,7 @@ enum class TftpOpcode : uint16_t {INVALID = 0, RRQ = 1, WRQ, DATA, ACK, ERROR, O
 enum class TftpMode {INVALID, OCTET};
 
 enum class TftpUserFacingErrorCode {ERR_NOERR = 0, ERR_UNSPECIFIED = 1, ERR_OACK_TIMEOUT = 2, ERR_OUTPUT_FILE_OPEN = 3,
-                                     ERR_REQUEST = 4,
+                                     ERR_REQUEST = 4, ERR_INPUT_FILE_OPEN = 5, ERR_WRONG_BLOCK = 6, ERR_OPCODE = 7,
                                      ERR_OPT_NEGOTIATION = 8};
 
 std::string error_message_from_code(TftpUserFacingErrorCode errorcode);
@@ -20,7 +20,7 @@ std::string error_message_from_code(TftpUserFacingErrorCode errorcode);
 using block_nr_t = uint16_t;
 using error_code_t = uint16_t;
 
-enum class TftpErrorCode : error_code_t {ERR_REQUSET = 4, ERR_OPT_NEGOTIATION = 8};
+enum class TftpErrorCode : error_code_t {ERR_FILE_NOT_FOUND = 1, ERR_ACCESS_VIOLATION = 2, ERR_DISK_FULL = 3, ERR_ILLEGAL_OP = 4, ERR_UNKNOWN_TR_ID = 5, ERR_FILE_EXISTS = 6, ERR_NO_SUCH_USER = 7, ERR_OPT_NEGOTIATION = 8};
 
 constexpr uint16_t CONTROLBYTES = 4;
 constexpr uint16_t OPCODELENGTH = 2;
@@ -33,7 +33,7 @@ constexpr uint16_t RETRANSMISSIONS_UNTIL_TIMEOUT = 4; //amount of resends before
 constexpr std::size_t DEFAULT_BLOCKSIZE = 512;
 
 //WORKAROUND!!!!
-constexpr uint16_t SERVER_LISTEN_PORT = 44500; //for debug: binding to port 69 does not work for some reason
+constexpr uint16_t SERVER_LISTEN_PORT = 44500; //for debug: binding to port 69 does not work without root privileges
 
 //Encapsules all possible options for a transmission
 //Gets set by client using option field in request
